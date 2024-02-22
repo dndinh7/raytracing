9cdd75b2c3422010ef85e985c4bcb38cb86b9900
David Dinh <dndinh7@gmail.com>
2024-02-21 21:53:36 -0600

    Got SDL working

--- raytracing/raytracing.cpp
+++ raytracing/raytracing.cpp
@@ -38,19 +38,31 @@ color ray_color(const ray& r) {
     return vec3::lerp(color(1.0, 1.0, 1.0), color(0.5, 0.7, 1.0), alpha);
 }
 
-int main() {
-    // Image
+int calculateHeight(double aspect_ratio, int width) {
+    // insight: aspect_ratio= width/height
 
+    int height = static_cast<int>(width / aspect_ratio);
+
+    // make sure height is never less than 1
+    return height < 1 ? 1 : height;
+}
+
+int main(int argc, char* argv[]) {
+    SDL_SetMainReady();
+
+    // Change width and aspect_ratio, then we will calculate the height based on those values
     double aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
+    int image_height = calculateHeight(aspect_ratio, image_width);
 
-    int image_height = static_cast<int>(image_width / aspect_ratio);
-    image_height = image_height < 1 ? 1 : image_height;
 
     // Camera
-
     float focal_length = 1.0;
+
+    // decrease to zoom in and increase to zoom out (FOV)
     float viewport_height = 2.0;
+
+    // using width / height instead of aspect ratio because height is truncated to nearest ratio and it can't be less than 1
     float viewport_width = viewport_height * (static_cast<float>(image_width) / image_height);
     vec3 camera_center = point3(0);
 
@@ -70,8 +82,57 @@ int main() {
     point3 pixel00_loc = viewpoint_upper_left + 0.5 * (pixel_du + pixel_dv); // offset by the half the size to get the mid point of pixel
 
 
+    // set up window
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "Failed to initialize the SDL2 library" << std::endl;
+        return -1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        image_width, image_height,
+        SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        std::cerr << "Failed to create window" << std::endl;
+        SDL_Quit();
+        return -1;
+    }
+
+    // set up renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "Could not create renderer!" << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    bool loop= true;
+    SDL_Event event;
+    
+    while (loop) {
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                loop = false;
+            }
+        }
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF); // Red
+
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+
     // Render
 
+    /*
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n"; 
 
     for (int j = 0; j < image_height; ++j) {
@@ -85,7 +146,7 @@ int main() {
             write_color(std::cout, pixel_color);
         }
     }
-
+    */
     std::clog << "\rDone.                          \n";
 
     return 0;
